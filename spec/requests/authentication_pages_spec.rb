d5dc7d147caecbbcd3baba2749e48c64b46e383a
Marcelo <marcelorpm@gmail.com>
2014-01-17 19:39:07 -0200

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -66,6 +66,16 @@ describe "Authentication" do
   				before { visit users_path }
   				it { should have_title('Sign in') }
   			end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
   		end
 
       describe "in the Microposts controller" do
@@ -84,6 +94,18 @@ describe "Authentication" do
   			before { patch user_path(user) }
   			specify { expect(response).to redirect_to(signin_path) }
   		end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
   	end
 
     describe "as non-admin user" do
